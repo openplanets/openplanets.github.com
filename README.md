dd7c5c030027a95045877aa35e2137c9eee4e7e3
Carl Wilson <carl@openplanetsfoundation.org>
2013-03-10 00:00:23 +0000

    Added check for README, LICENSE, and .opf.yaml.
    Script now retrieves root contents and checks for the the three
    required files in root.  Table cells are updated with the result.
    
    The bad news is that gathering this info pretty much exhausts the
    60 anonymous GitHub API requests allowed an hour from an IP address.

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 openplanets.github.com
 ======================
 
-Open Planets Foundation
+This is the Open Planets Foundation GitHub site project, which produces the web
+page that you see here http://openplanets.github.com.
+
+Currently the page is a test area for a test HTML/JavaScript page that uses the
+GitHub API to provide automated project status reporting.  The problem is the
+limit placed on anonymous calls (60 per hour), and the lack of JavaScript
+security, so it'll remain a test site to prototype ideas.
+
+The real reporting site to be done in Java, and run on the Jenkins box, can use
+OAuth. 
--- js/opf-github.js
+++ js/opf-github.js
@@ -12,8 +12,7 @@ if (!Date.prototype.toISODate) {
 }
 
 /**
- * Array prototype function to reduce an array to its
- * unique values.
+ * Array prototype function to reduce an array to its unique values.
  */
 Array.prototype.getUnique = function() {
 	var u = {}, a = [];
@@ -58,60 +57,73 @@ function displayOpfRepoTable() {
 			per_page : '100'
 		},
 		dataType : "jsonp",
-		// Callback for success, processes the 
-		// returned data, an array of repo objects
-		success : function(retData, message, jhXHR) {
-			// Sort the list by when last updated
-			retData.data.sort(function(a, b) {
-				if (a.updated_at > b.updated_at)
-					return -1;
-				if (a.updated_at < b.updated_at)
-					return 1;
-				return 0;
-			});
-			// Process the sorted array
-			repoArrayToTable(retData.data);
-			checkBasicPolicy(retData.data);
+		statusCode : {
+			200 : function(retData, message, jhXHR) {
+				// OK check the meta-field in the response
+				// and see if we've bust our limit
+				if (retData.meta.status == 403) {
+					alert(retData.data.message);
+				} else {
+					// Sort the list by when last updated
+					retData.data.sort(function(a, b) {
+						if (a.updated_at > b.updated_at)
+							return -1;
+						if (a.updated_at < b.updated_at)
+							return 1;
+						return 0;
+					});
+					// Process the sorted array
+					repoArrayToTable(retData.data);
+					$.each(retData.data, function(i, item) {
+						checkBasicPolicy(this);
+					});
+				}
+			}
 		}
 	});
 }
 
 /**
- * Takes the list of repos and creates the table rows and cells
- * needed to populate the table on the page.
+ * Takes the list of repos and creates the table rows and cells needed to
+ * populate the table on the page.
  * 
- * @param repos - the array of GitHub repo objects
+ * @param repos -
+ *            the array of GitHub repo objects
  */
 function repoArrayToTable(repos) {
 	// Lanuage array, and issue var for counters
-	var langs = ["Unknown"], issueCount = 0;
+	var langs = [ "Unknown" ], issueCount = 0;
 	// Process each repo in the array
 	$.each(repos, function(i, item) {
 		// Create a table row, with same id as the repo
-		var row = $('<tr>').attr({id : this.id.toString()});
+		var row = $('<tr>').attr({
+			id : this.id.toString()
+		});
 		var nameCell = $('<td>');
 		// Anchor that links to GitHub page, with repo name
 		// html value and description as title
 		var nameAnchor = $('<a>');
 		nameAnchor.attr({
-			href: 	this.html_url,
-			title:	this.description
+			href : this.html_url,
+			title : this.description
 		});
 		nameAnchor.html(this.name);
 		nameAnchor.appendTo(nameCell);
 		row.append(nameCell);
-		// Blanks for README and LICENSE
-		row.append('<td>No</td>');
-		row.append('<td>No</td>');
+		// Blanks for README, LICENSE, & OPF YAMl file
+		row.append('<td>no</td>');
+		row.append('<td>no</td>');
+		row.append('<td>no</td>');
 		// Cell for updated date
 		var updated = new Date(this.updated_at);
 		row.append('<td>' + updated.toISODate() + '</td>');
 		// Language, switch to unknown and add to array
-		if (this.language == null) this.language = 'Unknown'; 
+		if (this.language == null)
+			this.language = 'Unknown';
 		langs.push(this.language);
 		row.append('<td>' + this.language + '</td>');
 		// Add issue count to total and then the cell
-		issueCount+=this.open_issues;
+		issueCount += this.open_issues;
 		row.append('<td>' + this.open_issues + '</td>');
 		// Append to table nody
 		$('#repo-table > tbody').append(row);
@@ -125,30 +137,35 @@ function repoArrayToTable(repos) {
 }
 
 /**
- * Function to check contents of repo root for software
- * documentation required for all OPF projects:
- *   - README.md
- *       The GitHub README file.
- *   - LICENSE
- *       Statement of software license.
- *   - .opf.yml
- *       Basic project metadata.
- *       
- * @param repos - the list of repositories to check
+ * Function to check contents of repo root for software documentation required
+ * for all OPF projects: - README.md The GitHub README file. - LICENSE Statement
+ * of software license. - .opf.yml Basic project metadata.
+ * 
+ * @param repos -
+ *            the list of repositories to check
  * 
  */
-function checkBasicPolicy(repos) {
+function checkBasicPolicy(repo) {
 	// TODO: this processes the first item from the list
 	// for now to preserve precious GitHub API calls.
-	
 	// Call to get repo contents for root dir
 	$.ajax({
-		url :		gitHubURLs.repoContents(repos[0].url),
-		type :		"get",
-		dataType :	"jsonp",
-		success :	function(retData, message, jqXHR) {
-			// Success function currently empties the readme cell
-			$('#' + repos[0].id + ' > td:eq(1)').empty();
+		url : gitHubURLs.repoContents(repo.url),
+		type : "get",
+		dataType : "jsonp",
+		success : function(retData, message, jqXHR) {
+			// We have the contents for the repo root, now cycle
+			// through then checking for the required files
+			$.each(retData.data, function(i, item) {
+				if (this.type == "file") {
+					if (this.name == "README.md")
+						$('#' + repo.id + ' > td:eq(1)').html("yes");
+					else if (this.name == "LICENSE")
+						$('#' + repo.id + ' > td:eq(2)').html("yes");
+					else if (this.name == ".opf.yml")
+						$('#' + repo.id + ' > td:eq(3)').html("yes");
+				}
+			})
 		}
 	});
 }
